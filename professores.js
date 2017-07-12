54026df989b9019b6eac4d482e43c296064d2515
ANDRE LUIS MIRANDA <andre.lmiranda1@senacsp.edu.br>
2017-07-12 12:41:57 -0300

    Aula com lista de dados

--- professores.js
+++ professores.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
 
+    // Inicializar os serviços
     $(".button-collapse").sideNav();
     $('.modal').modal();
     $('select').material_select();
@@ -7,13 +8,22 @@ $(document).ready(function () {
         selectMonths: true,
         selectYears: 3
     });
-});
+     
+      inicializarDados ();
+});      
+   
+
+
+
+
+        
+
 
 $("#salvarBtn").click(function () {
 
     var professor = {
-        nome: $("#nomme").val(),
-        idade: (parseint$("#idade").val()),
+        nome: $("#nome").val().trim(),
+        idade: parseInt($("#idade").val().trim()),
         genero: $("input[name=genero]:checked").val(),
         formacao: $("#formacao").val(),
         areaAtuacao: $("#areaAtuacao").val(),
@@ -24,23 +34,74 @@ $("#salvarBtn").click(function () {
     var valido = validar(professor);
 
     if (valido) {
-        //adicionar na tabela
+
+        adicionarProfessorNaTabela(professor);
+        Materialize.toast("Professor cadastrado!", 5000);
+        $ ("#modalAdicionar").modal("close");
+        $ ("#cadastroProfesssor").trigger("reset");
     } else {
         // mensagem de erro
+        
+        Materialize.toast("Preencha os campos obrigatórios", 3000);
     }
 });
 
 function validar(professor) {
 
+    //false, 0,"",undefined, null
     if (professor.nome &&
         professor.idade &&
         professor.genero &&
         professor.formacao &&
-        professor.areaAtuacao &&) {
+        professor.areaAtuacao) {
       
       return true;
         } else {
             return false;
         }
+
+}
+ var contador = 0;
+
+function adicionarProfessorNaTabela(professor) {
+    contador++;
+    var idvalue ="row" + contador;
+    
+    $("#corpoTabela").append("<tr>" + 
+                                     "<td>" +
+                                     "<input type='radio' name='edicao' id='" + idvalue + "'value'" + idvalue +"'>" +
+                                    "<label for= '" + idvalue + "'></label>" +
+                                    "</td>" +
+                                     "<td>" + professor.nome +"</td>" +
+                                      "<td>" + professor.idade +"</td>" +
+                                      "<td>" + professor.formacao +"</td>" +
+                                      "<td>" + professor.areaAtuacao +"</td>" +
+                                      "<td>" + professor.disponibilidadedeInicio +"</td>" +
+                                      "<td>" + professor.disponibilidadedeFim +"</td>" +
+                                      "</tr>");
+
+}
+
+$("#removerBtn").click(function(){
+
+    var selecionado = $("input[name=edicao]:checked");
+
+    if (selecionado.val()) {
+        // Lógica para remover
+        selecionado.parent().parent().remove();
+        Materialize.toast("Removido com sucesso",5000);
+    } else {
+         Materialize.toast("Selecione um registro para remover!", 5000);
+    }
+
+});
+
+function inicializarDados() {
+
+    var listaProfessores = JSON.parse(dados);
+    
+    listaProfessores.forEach(function (professor) {
+      adicionarProfessorNaTela(professor);  
+    });
 
 }
---
+++ professores_dados.js
@@ -0,0 +1,25 @@
+var dados =`
+[
+    {
+        "nome": "Jeferson",
+        "idade": 28,
+        "genero": "Masculino",
+        "formação": "Superior",
+        "areaAtuacao": "",
+        "disponibilidadeInico": null,
+        "disponibilidadeFim": null
+    },
+
+     {
+        "nome": "Fulano",
+        "idade": 28,
+        "genero": "Masculino",
+        "formação": "Pós-graduado",
+        "areaAtuacao": "",
+        "disponibilidadeInico": null,
+        "disponibilidadeFim": null
+    }
+
+
+
+]`;
